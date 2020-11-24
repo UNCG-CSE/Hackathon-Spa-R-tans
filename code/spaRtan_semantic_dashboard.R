1c52f8cda4810afe4dd7e573497cb020c44eebcc
mowgli <46568362+mowgl-i@users.noreply.github.com>
2020-11-23 22:19:41 -0500

    push

--- code/spaRtan_semantic_dashboard.R
+++ code/spaRtan_semantic_dashboard.R
@@ -189,9 +189,6 @@ customTheme <- shinyDashboardThemeDIY(
 
 
 # Define UI ----------
-`+.uneval` <- function(a,b) {
-  `class<-`(modifyList(a,b), "uneval")
-}
 
 # What does the dashboard look like?
 
@@ -213,9 +210,8 @@ ui <- dashboardPage(#skin = "blue" , # find appropriate uncg color?
       
       # First arg is what is seen on the dashboard, Second indicates to output where to show output
       # icon is not needed, just adds a little icon. 
-      menuItem("Task 1: ", tabName = "task_1",icon = icon("signal")),
-      menuItem("Task 2: What are we supposed to do?", tabName = "task_2", icon = icon("dashboard")),
-      menuItem("Datatable: View some data",  tabName = "Data_Table", icon = icon("signal"))
+      menuItem("Task 1.1: ", tabName = "task_1",icon = icon("signal")),
+      menuItem("Task 1.2: What are we supposed to do?", tabName = "task_1_2", icon = icon("dashboard"))
       
     )
   ),
@@ -227,19 +223,19 @@ ui <- dashboardPage(#skin = "blue" , # find appropriate uncg color?
     tabItems(
       tabItem("task_1",
               h1("Static plot of energy consumption"),
-              box(selectInput("meter_choice_box_1","Choose Building",meter_choices, selected = "Elliott University Center (040) - Main Meter", multiple = T),
+              box(width = 10,title = "Select Plot Parameters",selectInput("meter_choice_box_1","Choose Building",meter_choices, selected = "Elliott University Center (040) - Main Meter", multiple = T),
                   selectInput("agg_level", "Choose sum or mean energy",
                               c("Mean_Energy_Actual","Mean_Energy_Predicted",
                                 "Total_Energy_Actual", "Total_Energy_Predicted"), selected = "Total_Energy_Predicted"),
-                  wellPanel(
-                  dateRangeInput("daterange_1", "Filter by date", start = as.Date(start_date), end = as.Date(end_date)),
-                  box(plotOutput("task_1.1_plot"),width = "auto")),
                   selectInput("time_choice_box_1", "Choose Time aggredation" , c("year",
                                                                                  "month","day","hour"),
-                              selected = "month"))),
+                              selected = "month")),
+              wellPanel(
+                dateRangeInput("daterange_1", "", start = as.Date(start_date), end = as.Date(end_date)),
+                plotOutput("task_1.1_plot"),width = "auto")),
               #box(plotOutput("task_1.1_plot"),width = "auto")),
       
-    tabItem("task_2",# this links back to the sidebar item :) 
+    tabItem("task_1_2",# this links back to the sidebar item :) 
             
             h1("Create a real-time interactive plot of energy consumption and prediction"), ## adding simple text...
             h3("1.1 Static Plot"),
@@ -257,23 +253,14 @@ ui <- dashboardPage(#skin = "blue" , # find appropriate uncg color?
                                                                              "Week of the year","Month"), selected = "Month")),
             box(selectInput("meter_choice_box_3","Choose builing to display", meter_choices, selected = "Elliott University Center (040) - Main Meter", multiple = T)),
             
-            box(plotOutput("meter_choice_plot_3"), width = "auto")),
-    
-    tabItem("Data_Table", # leaving this page empty for now
-            
-            h1("this is an empty page"),
-            box(selectInput("meter_choice_box_4","Choose builing to display", meter_choices, selected = "Elliott University Center (040) - Main Meter", multiple = T)),
-            DT::dataTableOutput("thing")
-            
-    )
-  )
+            box(plotOutput("task_1.2_plot"), width = "auto")))
 ))
 
 server <- function(input,output){
   
   
   # Reeactive function to filter the dataset
-  data_1 <- reactive({ # this is referenced in the ggplot. 
+  data_1.2 <- reactive({ # this is referenced in the ggplot. 
     data <- combined_results %>% filter(better_label == input$meter_choice_box_3,
                                         Datetime >= input$daterange_1[1] & Datetime <= input$daterange_1[2]) %>% 
       select("Actual","Predicted","Hour",input$time_choice_box_3,"better_label") %>% 
@@ -333,9 +320,9 @@ server <- function(input,output){
   
   # this plot will go the the "task_1" page :)
   
-  output$meter_choice_plot_3 <- renderPlot({ # render a plot, the meter_choice_plot, which is found in task_1 tab
+  output$task_1.2_plot <- renderPlot({ # render a plot, the meter_choice_plot, which is found in task_1 tab
     
-    ggplot(data_1(),aes(x = Time_Label, color = better_label)) + # ggplot, year on x axis
+    ggplot(data_1.2(),aes(x = Time_Label, color = better_label)) + # ggplot, year on x axis
       geom_line(aes(y = `Mean_Energy_Actual`),show.legend = T)+ # actual both on y axis
       geom_line(aes(y = `Mean_Energy_Predicted`), linetype = "dashed",show.legend = T)+ # predict
       theme_minimal()+ # random theme
@@ -344,11 +331,7 @@ server <- function(input,output){
     
   })
   
-  output$thing <- renderDataTable({
-    data_2 <- data_2() 
-    
-    data_2
-  })
+
   
 }
 
